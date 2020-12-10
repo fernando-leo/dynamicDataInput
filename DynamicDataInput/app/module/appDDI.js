4f949142792a8b76bf3e1ca99905823273e89184
Fernando Ruffino <fernando.ruffino@scanbrazilconsulting.com>
2020-12-10 17:50:08 -0300

    primeiro commit

---
+++ DynamicDataInput/app/module/appDDI.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('appDDI')
+	.config(function ($routeProvider, $locationProvider) {
+
+
+		$routeProvider.when('/', { templateUrl: 'app/view/index.html', controller: 'ddiController' }),
+			$routeProvider.when('/edit', { templateUrl: 'app/view/edit.html', controller: 'ddiEditController' })
+		$routeProvider.otherwise({ redirectTo: '/home' });
+
+	}).run(function ($rootScope, $location, $log, $http) {
+		$rootScope.title = '';
+
+	});
